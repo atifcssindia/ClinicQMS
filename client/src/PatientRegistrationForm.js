6888cc03ea0d091b30b962905eeb2a7db9ae2de5
Fuel-Free <128348584+Fuel-Free@users.noreply.github.com>
2023-11-30 18:12:25 +0530

    updating node

--- client/src/PatientRegistrationForm.js
+++ client/src/PatientRegistrationForm.js
@@ -36,7 +36,7 @@ const PatientRegistrationForm = () => {
         };
 
         try {
-            const response = await fetch('http://localhost:5001/register', {
+            const response = await fetch('`${process.env.REACT_APP_API_URL}/register`', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
