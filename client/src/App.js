e20a7c54acabf8869478cc648ae85bda9b3dfdc5
atifcssindia <39831522+atifcssindia@users.noreply.github.com>
2023-12-04 18:59:28 +1100

    Login and tailwind setup.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import PatientRegistrationForm from './PatientRegistrationForm';
-import Login from './Login';
-import DoctorView from './doctorview';
-import Registration from './Registration'; // Import your new component
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PatientRegistrationForm from "./PatientRegistrationForm";
+import Login from "./Login";
+import DoctorView from "./doctorview";
+import Registration from "./Registration"; // Import your new component
+import "./main.css";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/registration" element={<Registration />} />
-                <Route path="/" element={<PatientRegistrationForm />} />
-                <Route path="/doctorview" element={<DoctorView />} /> {/* Add this line */}
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/" element={<PatientRegistrationForm />} />
+        <Route path="/doctorview" element={<DoctorView />} />{" "}
+        {/* Add this line */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- client/src/Login.js
+++ client/src/Login.js
@@ -1,83 +1,133 @@
-import React, { useState } from 'react';
-import { TextField, Button } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { TextField, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const handleLogin = async (event) => {
     event.preventDefault();
     // Perform validation if needed
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-      });
-      
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/auth/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        }
+      );
+
       const data = await response.json();
       if (response.ok) {
         // Store the token in local storage or in-memory storage as preferred
-        localStorage.setItem('token', data.token);
-        console.log('here is data', data);
+        localStorage.setItem("token", data.token);
+        console.log("here is data", data);
         // Redirect based on the role
         switch (data.role) {
-          case 'doctor':
-            navigate('/doctorview');
+          case "doctor":
+            navigate("/doctorview");
             break;
-          case 'receptionist':
-            navigate('/receptionview');
+          case "receptionist":
+            navigate("/receptionview");
             break;
           default:
             // Handle other roles or lack thereof
-            throw new Error('Unknown role');
+            throw new Error("Unknown role");
         }
       } else {
         // Handle errors, such as showing an alert to the user
-        throw new Error(data.message || 'Login failed');
+        throw new Error(data.message || "Login failed");
       }
     } catch (error) {
       // Handle errors
-      console.error('Login error:', error);
-      alert(error.message || 'Network error');
+      console.error("Login error:", error);
+      alert(error.message || "Network error");
     }
   };
 
+  // const backgroundImageAuth = "./auth-side-bg.jpg";
+
   return (
-    <div style={{ padding: '20px' }}>
-      <form onSubmit={handleLogin} style={{ marginBottom: '20px' }}>
-        <TextField
-          label="Email"
-          type="email"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <TextField
-          label="Password"
-          type="password"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button type="submit" variant="contained" color="primary" fullWidth>
-          Login
-        </Button>
-      </form>
-      <Button
-        variant="text"
-        onClick={() => navigate('/Registration')}
-      >
-        Don't have an account? Register
-      </Button>
+    <div className="app-layout-blank flex flex-auto flex-col h-[100vh]">
+      <div className="grid lg:grid-cols-3 h-full">
+        <div
+          style={{
+            backgroundImage: `url("images/auth-side-bg.jpg")`,
+          }}
+          className="bg-no-repeat bg-cover py-6 px-16 flex-col justify-between hidden lg:flex  relative"
+        >
+          <div className="logo text-5xl text-white">ClinicQMS</div>
+
+          <div>
+            {/* <div className="mb-6 flex items-center gap-4">
+              <span className="avatar avatar-circle avatar-md border-2 border-white"></span>
+              <div className="text-white">
+                <div className="font-semibold text-base">Brittany Hale</div>
+                <span className="opacity-80">CTO, Onward</span>
+              </div>
+            </div> */}
+            <p className="text-lg text-white opacity-80">
+              Elstar comes with a complete set of UI components crafted with
+              Tailwind CSS, it fulfilled most of the use case to create modern
+              and beautiful UI and application
+            </p>
+          </div>
+
+          <span className="text-white">
+            Copyright © 2023 <span className="font-semibold">CSS India</span>{" "}
+          </span>
+        </div>
+
+        <div className="col-span-2 flex flex-col justify-center items-center bg-white dark:bg-gray-800">
+          <div className="xl:w-6/12 px-8">
+            <div className="mb-8">
+              <h3 className="mb-1 text-xl font-bold">Welcome back!</h3>
+              <p className="text-gray-600">
+                Please enter your credentials to sign in!
+              </p>
+            </div>
+
+            <div style={{ padding: "0" }}>
+              <form onSubmit={handleLogin} style={{ marginBottom: "20px" }}>
+                <TextField
+                  label="Email"
+                  type="email"
+                  variant="outlined"
+                  fullWidth
+                  margin="normal"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <TextField
+                  label="Password"
+                  type="password"
+                  variant="outlined"
+                  fullWidth
+                  margin="normal"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                >
+                  Login
+                </Button>
+              </form>
+              <Button variant="text" onClick={() => navigate("/Registration")}>
+                Don't have an account? Register
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
