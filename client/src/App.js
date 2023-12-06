68baf9cca12cc43247e057406f9c5d357a220d74
atifcssindia <39831522+atifcssindia@users.noreply.github.com>
2023-12-06 23:36:47 +1100

    QR code push static

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import DoctorView from "./doctorview";
 import Registration from "./Registration"; // Import your new component
 import "./main.css";
 import { SidebarProvider } from "./services/SidebarContext";
+import QR from "./components/QR";
 
 function App() {
   return (
@@ -15,8 +16,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/registration" element={<Registration />} />
           <Route path="/" element={<PatientRegistrationForm />} />
-          <Route path="/doctorview" element={<DoctorView />} />{" "}
-          {/* Add this line */}
+          <Route path="/doctorview/*" element={<DoctorView />} />
+          <Route path="/qr/*" element={<QR />} />
         </Routes>
       </Router>
     </SidebarProvider>
--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -49,9 +49,19 @@ const SideBar = () => {
                   hover:after:w-1  hover:after:bg-blue-600  hover:after:h-full  hover:after:absolute   hover:after:inline-flex  hover:after:left-0 hover:bg-[#212121]/10
                 flex gap-x-2 items-center text-md leading-[42px] min-h-[42px] h-auto text-[#424242] no-underline pl-5 clear-both border-l-4 border-transparent whitespace-nowrap"
               >
-                <i className="inline-block w-45 h-34 leading-34 text-center top-0 relative z-10">
+                <i className="inline-block w-45 h-34 leading-34 text-center top-0 relative z-10 text-xl text-blue-800">
                   {" "}
-                  <img src="images/navbar/1.png" alt="" className=" w-5" />
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 448 512"
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M400 64h-48V12c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v52H160V12c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v52H48C21.49 64 0 85.49 0 112v352c0 26.51 21.49 48 48 48h352c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm-6 400H54a6 6 0 0 1-6-6V160h352v298a6 6 0 0 1-6 6zm-52.849-200.65L198.842 404.519c-4.705 4.667-12.303 4.637-16.971-.068l-75.091-75.699c-4.667-4.705-4.637-12.303.068-16.971l22.719-22.536c4.705-4.667 12.303-4.637 16.97.069l44.104 44.461 111.072-110.181c4.705-4.667 12.303-4.637 16.971.068l22.536 22.718c4.667 4.705 4.636 12.303-.069 16.97z"></path>
+                  </svg>
                 </i>
                 {isSidebarOpen ? (
                   ""
@@ -62,13 +72,24 @@ const SideBar = () => {
             </li>
             <li className=" overflow-x-hidden  relative">
               <Link
-                to="/dashboard"
+                to="/qr"
                 className="
                 hover:after:w-1  hover:after:bg-blue-600  hover:after:h-full  hover:after:absolute   hover:after:inline-flex  hover:after:left-0 hover:bg-[#212121]/10
               flex gap-x-2 items-center text-md leading-[42px] min-h-[42px] h-auto text-[#424242] no-underline pl-5 clear-both border-l-4 border-transparent whitespace-nowrap"
               >
-                <i className="inline-block w-45 h-34 leading-34 text-center top-0 relative">
-                  <img src="images/navbar/6.png" alt="" className=" w-5" />
+                <i className="inline-block w-45 h-34 leading-34 text-center top-0 relative text-xl text-blue-900">
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 24 24"
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path fill="none" d="M0 0h24v24H0z"></path>
+                    <path d="M9.5 6.5v3h-3v-3h3M11 5H5v6h6V5zm-1.5 9.5v3h-3v-3h3M11 13H5v6h6v-6zm6.5-6.5v3h-3v-3h3M19 5h-6v6h6V5zm-6 8h1.5v1.5H13V13zm1.5 1.5H16V16h-1.5v-1.5zM16 13h1.5v1.5H16V13zm-3 3h1.5v1.5H13V16zm1.5 1.5H16V19h-1.5v-1.5zM16 16h1.5v1.5H16V16zm1.5-1.5H19V16h-1.5v-1.5zm0 3H19V19h-1.5v-1.5zM22 7h-2V4h-3V2h5v5zm0 15v-5h-2v3h-3v2h5zM2 22h5v-2H4v-3H2v5zM2 2v5h2V4h3V2H2z"></path>
+                  </svg>
                 </i>
                 {isSidebarOpen ? "" : <span> QR Code</span>}
               </Link>
