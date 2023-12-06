095623827b770c9c65ba89d6aa77c016a15d4380
atifcssindia <39831522+atifcssindia@users.noreply.github.com>
2023-12-06 20:43:05 +1100

    Update Header and side menu

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,21 @@ import Login from "./Login";
 import DoctorView from "./doctorview";
 import Registration from "./Registration"; // Import your new component
 import "./main.css";
+import { SidebarProvider } from "./services/SidebarContext";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/registration" element={<Registration />} />
-        <Route path="/" element={<PatientRegistrationForm />} />
-        <Route path="/doctorview" element={<DoctorView />} />{" "}
-        {/* Add this line */}
-      </Routes>
-    </Router>
+    <SidebarProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/" element={<PatientRegistrationForm />} />
+          <Route path="/doctorview" element={<DoctorView />} />{" "}
+          {/* Add this line */}
+        </Routes>
+      </Router>
+    </SidebarProvider>
   );
 }
 
--- client/src/doctorview.js
+++ client/src/doctorview.js
@@ -4,16 +4,19 @@ import { jwtDecode } from "jwt-decode";
 import AppointmentsTable from "./AppointmentsTable";
 import Header from "./components/header";
 import SideBar from "./components/SideBar";
-import { Button } from '@mui/material';
+import { Button } from "@mui/material";
+import { useSidebar } from "./services/SidebarContext";
 
 const DoctorView = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
   const [appointments, setAppointments] = useState([]);
 
+  const { isSidebarOpen, toggleSidebar } = useSidebar();
+
   const fetchAppointments = async (decodedToken) => {
     const token = localStorage.getItem("token");
-  
+
     try {
       const response = await fetch(
         `${process.env.REACT_APP_API_URL}/appointments/today?userId=${decodedToken.user_id}`,
@@ -25,7 +28,7 @@ const DoctorView = () => {
           },
         }
       );
-  
+
       if (response.ok) {
         const appointmentsData = await response.json();
         setAppointments(appointmentsData);
@@ -40,7 +43,7 @@ const DoctorView = () => {
   const handleNextPatient = async () => {
     const token = localStorage.getItem("token");
     const decodedToken = jwtDecode(token);
-  
+
     try {
       const response = await fetch(
         `${process.env.REACT_APP_API_URL}/appointments/next`,
@@ -53,7 +56,7 @@ const DoctorView = () => {
           body: JSON.stringify({ userId: decodedToken.user_id }),
         }
       );
-  
+
       if (response.ok) {
         await fetchAppointments(decodedToken); // Re-fetch appointments after updating the status
       } else {
@@ -93,36 +96,32 @@ const DoctorView = () => {
     return null; // or <LoadingIndicator />;
   }
 
-  
-
-
-  
-  
-
   return (
     <div className="app-layout-classic flex flex-auto flex-col  bg-[#f3f4f6]">
       <div className="flex flex-auto min-w-0">
-        <SideBar />
-        <div className="flex flex-col flex-auto min-h-screen min-w-0 relative w-full">
-          <Header />
+        <SideBar isOpen={isSidebarOpen} />
+        <div className="flex flex-col flex-auto min-h-screen min-w-0 relative w-full  transition-width duration-600">
+          <Header toggleSidebar={toggleSidebar} />
           <div className="h-full flex flex-auto flex-col">
             <div className="h-full flex flex-auto flex-col justify-between">
               <main className="h-full">
                 <div className="page-container relative h-full flex flex-auto flex-col px-4 sm:px-6 py-4 sm:py-6 md:px-8">
                   <h1 className=" text-xl font-semibold text-gray-800 mb-2">
-                    Doctor Dashboard
+                    Appointments
                   </h1>
                   <AppointmentsTable appointments={appointments} />{" "}
                   {/* Render the table */}
-                  <div className="mt-4"> {/* Add some margin to the top */}
-                  <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handleNextPatient}
-                  className="self-end" // Tailwind class to align the button to the end of its container
-                  >
-                  Next Patient
-                  </Button>
+                  <div className="mt-4">
+                    {" "}
+                    {/* Add some margin to the top */}
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={handleNextPatient}
+                      className="self-end" // Tailwind class to align the button to the end of its container
+                    >
+                      Next Patient
+                    </Button>
                   </div>
                 </div>
               </main>
