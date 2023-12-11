42081f8054ab98cef9e344f3fc9ee5285ee717aa
Fuel-Free <128348584+Fuel-Free@users.noreply.github.com>
2023-12-11 21:02:35 +0530

    adding dev.vitalx.in

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const axios = require('axios');
 require('dotenv').config();
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://thriving-bonbon-27d691.netlify.app', 'https://app.vitalx.in'],
+  origin: ['http://localhost:3000', 'https://thriving-bonbon-27d691.netlify.app', 'https://app.vitalx.in', 'https://dev.vitalx.in', 'https://admirable-taiyaki-b2b323.netlify.app'],
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
